dee3de5bfb0c6cc8bbd026c90f276d268aea8ea4
Paul Kinlan <paul.kinlan@gmail.com>
2024-08-14 16:49:23 +0900

    First commit
    Importing reactive agent from reactive-prompt demo folder

---
+++ vite.config.js
@@ -0,0 +1,37 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [],
+  server: {
+    host: "0.0.0.0",
+    hmr: true, // Change this line to false disable auto-refreshing.
+  },
+  resolve: {
+    alias: {
+      "@paulkinlan/reactive-agent": resolve(__dirname, "lib"),
+    },
+  },
+  build: {
+    lib: {
+      formats: ["es"],
+      entry: {
+        index: resolve(__dirname, "lib/index.js"),
+      },
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["@preact/signals-core", "@paulkinlan/reactive-prompt"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          signalsCore: "@preact/signals-core",
+        },
+      },
+    },
+    soucemap: true,
+  },
+});
